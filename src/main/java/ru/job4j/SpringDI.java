0f0884890c29cab2b191f857fb11cb9bea3ccc8c
Sergei Baranov <mariosb84@mail.ru>
2023-06-15 11:41:04 +0300

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/ru/job4j/SpringDI.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+    }
+}
